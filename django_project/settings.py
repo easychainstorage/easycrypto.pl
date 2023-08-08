531f56b57347f96bb015471df494296bb1af64a7
bbojan <bartlomiej.piotr.bojanowski@gmail.com>
2023-08-08 11:32:35 +0200

    Fix mobile navbar

--- django_project/settings.py
+++ django_project/settings.py
@@ -28,8 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = env.bool("DEBUG", default=False)
-DEBUG=True
+DEBUG = env.bool("DEBUG", default=False)
+#DEBUG=True
 ALLOWED_HOSTS = ['easycrypto.pl', 'www.easycrypto.pl', '0.0.0.0']
 
 SITE_ID = 1
